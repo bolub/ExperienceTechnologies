b9cf4ef9152da0282c618465eef0429d94ee2392
bolub <abiol5202@gmail.com>
2019-08-07 23:25:18 +0100

    Modified

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express           = require('express');
 const app               = express();
 const PORT              = process.env.PORT || 5000;
-const path              = require('path');
 
 //use public folder
 app.use(express.static(__dirname + '/public'));
